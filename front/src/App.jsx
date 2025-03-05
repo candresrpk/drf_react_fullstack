461f642dd13913295a71b0da47c80810d0c856c3
Carlos Reyes <andrespk3000@gmail.com>
2025-03-04 23:23:02 -0500

    protected routed finished

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,46 @@
-import { useState } from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+const logOut = () => {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+};
+
+const RegisterAndLogout = () => {
+  localStorage.clear();
+  return <Register />;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="/logout" element={<logOut />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- front/src/components/ProtectedRoute.jsx
+++ front/src/components/ProtectedRoute.jsx
@@ -2,14 +2,49 @@ import { Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ProtectedRoute({ children }) {
   const [isAurhorized, setIsAurhorized] = useState(false);
 
-  const refreshToken = async () => {};
+  useEffect(() => {
+    auth().catch(() => setIsAurhorized(false));
+  }, []);
 
-  const auth = async () => {};
+  const refreshToken = async () => {
+    const refreshT = localStorage.getItem(REFRESH_TOKEN);
+    try {
+      const res = await api.post("/api/accounts/refresh/", {
+        refresh: refreshT,
+      });
+      if (res.status === 200) {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        setIsAurhorized(true);
+      } else {
+        setIsAurhorized(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsAurhorized(false);
+    }
+  };
+
+  const auth = async () => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (!token) {
+      setIsAurhorized(false);
+      return;
+    }
+    const decoded = jwtDecode(token);
+    const tokenExpiration = decoded.exp;
+    const now = Date.now() / 1000;
+
+    if (tokenExpiration < now) {
+      await refreshToken();
+    } else {
+      setIsAurhorized(true);
+    }
+  };
 
   if (isAurhorized == null) {
     return (
@@ -21,3 +56,5 @@ function ProtectedRoute({ children }) {
 
   return isAurhorized ? children : <Navigate to="/login" />;
 }
+
+export default ProtectedRoute;
